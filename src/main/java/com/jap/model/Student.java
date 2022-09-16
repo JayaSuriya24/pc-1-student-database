d0ac016aecae90e5dd2918b5bebdb56af8fe5dc1
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-16 17:45:30 +0530

    fields for student class is created

--- src/main/java/com/jap/model/Student.java
+++ src/main/java/com/jap/model/Student.java
@@ -3,7 +3,10 @@
  * Date: 9/16/2022
  * Created With: IntelliJ IDEA Community Edition
  */
-package com.jap;
+package com.jap.model;
 
 public class Student {
+    private int roll_number;
+    private String name;
+    private char grade;
 }
