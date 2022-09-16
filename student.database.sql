ce5ece74d1d16f22cf13f1b500cff8bfe80f7ee2
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-16 17:39:08 +0530

    query to insert all the values in the table

--- student.database.sql
+++ student.database.sql
@@ -10,3 +10,14 @@ CREATE TABLE IF NOT EXISTS `school`.`student`(
 );
 --query to select all the values in the table
 SELECT * FROM school.student;
+-- query to insert the values inside the table for all the fields
+--query for rollNumber 624
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`) VALUES ('624', 'suriya', 'a');
+--query for rollNumber 625
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`) VALUES ('625', 'ajay', 'b');
+--query for rollNumber 626
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`) VALUES ('626', 'stanley', 'c');
+--query for rollNumber 627
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`) VALUES ('627', 'sugeen', 'c');
+--query for rollNumber 628
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`) VALUES ('628', 'hari', 'a');
