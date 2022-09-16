fd86f4573c0806e91fe415881bad576efe0da201
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-16 17:31:55 +0530

    student table is created

--- student.database.sql
+++ student.database.sql
@@ -2,3 +2,9 @@
 CREATE DATABASE IF NOT  EXISTS `school`;
 --use the school database
 USE `school`;
+--create the table called name student
+CREATE TABLE IF NOT EXISTS `school`.`student`(
+`roll_number` int not null primary key ,
+`name` varchar(40) not null,
+`grade` char not null
+);
